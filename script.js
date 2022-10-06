ee28bc3df1b5938591e7312e112c8814f580e994
Anish <113669810+radlysamar@users.noreply.github.com>
2022-10-06 06:01:25 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.console.log(window)
+console.log(document.body)
+document.body.style.background ="red"
+
+bom
+// alert and prompt//are come under bom
+// location.href ="https://darkcoders.ml/"
